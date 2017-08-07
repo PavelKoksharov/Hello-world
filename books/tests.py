6fd66d7f9de4670023dac53d4e792a6661ab14b8
Pavel <pavel@localhost.localdomain>
2017-08-07 12:16:10 +0500

    First commit

---
+++ books/tests.py
@@ -0,0 +1,16 @@
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from .models import Book
+
+class BookMethodTests(TestCase):
+
+    def test_recent_pub(self):
+        """
+        recent_publication() should return False for future publication
+        dates.
+        """
+
+        futuredate = timezone.now().date() + datetime.timedelta(days=5)
+        future_pub = Book(publication_date=futuredate)
+        self.assertEqual(future_pub.recent_publication(), False)
---
+++ books/urls.py
@@ -0,0 +1,4 @@
+urlpatterns = [
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
+]
